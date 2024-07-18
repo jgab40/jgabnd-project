f3d105255a488491823d2f533a3a45ed18b5f524
jagb40 <jg.ndongendeba@gmail.com>
2024-07-18 20:57:10 +0200

    xml ok

--- script.js
+++ script.js
@@ -1,2 +1,50 @@
+// Opens new browser windows
+function openGitWindow() {
+    window.open("https://github.com/jgab40", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+}
+function openLinkedinWindow() {
+    window.open("https://www.linkedin.com/in/jgndongendeba/details/certifications/", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+}
 
+// Navigate Between my skills catalog
+let i = 0;
+let x;
+displayCD(i);
+
+function displayCD(i) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            myFunction(this, i);
+        }
+    };
+    xmlhttp.open("GET", "cd_catalog.xml", true);
+    xmlhttp.send();
+}
+
+function myFunction(xml, i) {
+    var xmlDoc = xml.responseXML; 
+    x = xmlDoc.getElementsByTagName("CD");
+    document.getElementById("showCD").innerHTML =
+    "Skills covered: " +
+    x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
+    "<br>Certifying organizations: " +
+    x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+    "<br>Year: " + 
+    x[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue;
+}
+
+function next() {
+if (i < x.length-1) {
+  i++;
+  displayCD(i);
+  }
+}
+
+function previous() {
+if (i > 0) {
+  i--;
+  displayCD(i);
+  }
+}
 
