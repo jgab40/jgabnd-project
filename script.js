493a239fe05308afadddfe9cdfd6ab528ce5b519
jagb40 <jg.ndongendeba@gmail.com>
2024-08-27 21:46:45 +0200

    Updated index.html with a IIFE

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// Greetings
-function myGreetings() {
-    let text = "";
-    const d = new Date();
-    const time = d.getHours();
-    if (time < 10) {
-        text = `Good Morning!`;
-      } else if (time < 18) {
-        text = `Good Day!`;
-      } else {
-        text = `Good Evening!`;
-      }
-    document.getElementById("greeting").innerHTML = text;
-}
-
 // Opens new browser windows
 let myGitWindow;
 let myLinkedinWindow;
@@ -35,39 +20,39 @@ let x;
 displaySkill(i);
 
 function displaySkill(i) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            myLoadFunction(this, i);
-        }
-    };
-    xmlhttp.open("GET", "skills_catalog.xml", true);
-    xmlhttp.send();
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      myLoadFunction(this, i);
+    }
+  };
+  xmlhttp.open("GET", "skills_catalog.xml", true);
+  xmlhttp.send();
 }
 
 function myLoadFunction(xml, i) {
-    var xmlDoc = xml.responseXML; 
-    x = xmlDoc.getElementsByTagName("SKILL");
-    document.getElementById("showSkill").innerHTML =
+  var xmlDoc = xml.responseXML;
+  x = xmlDoc.getElementsByTagName("SKILL");
+  document.getElementById("showSkill").innerHTML =
     "Skills covered: " +
     x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
     "<br>Certifying organizations: " +
     x[i].getElementsByTagName("CERTIF")[0].childNodes[0].nodeValue +
-    "<br>Year: " + 
+    "<br>Year: " +
     x[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue;
 }
 
 function next() {
-if (i < x.length-1) {
-  i++;
-  displaySkill(i);
+  if (i < x.length - 1) {
+    i++;
+    displaySkill(i);
   }
 }
 
 function previous() {
-if (i > 0) {
-  i--;
-  displaySkill(i);
+  if (i > 0) {
+    i--;
+    displaySkill(i);
   }
 }
 
