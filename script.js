32f993f2c65036706b82cb309aaf4e47545a3a2e
jagb40 <jg.ndongendeba@gmail.com>
2024-08-29 15:39:19 +0200

    inserted defer on script tag

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+//Greetings
+(function () {
+  let text = "";
+  const d = new Date();
+  const time = d.getHours();
+  if (time < 10) {
+    text = `Good Morning!`;
+  } else if (time < 18) {
+    text = `Good Day!`;
+  } else {
+    text = `Good Evening!`;
+  }
+  document.getElementById("greeting").innerHTML = text;
+}());
+
 // Opens new browser windows
 let myGitWindow;
 let myLinkedinWindow;
