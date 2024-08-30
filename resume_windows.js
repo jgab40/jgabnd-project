5d3fcd4fe4586787cd9b4ad0b213a5eadb06be67
jagb40 <jg.ndongendeba@gmail.com>
2024-08-30 17:51:19 +0200

    Ok

--- resume_windows.js
+++ resume_windows.js
@@ -1,23 +1,23 @@
 // Opens new browser windows
 let myGitWindow;
 let myLinkedinWindow;
-function openGitWindow() {
+const openGitWindow = () => {
   myGitWindow = window.open(
     "https://github.com/jgab40",
     "_blank",
     "toolbar=yes,scrollbars=yes,resizable=yes,top=150,left=150,width=400,height=200"
   );
-}
-function closeGitWindow() {
+};
+const closeGitWindow = () => {
   myGitWindow.close();
-}
-function openLinkedinWindow() {
+};
+const openLinkedinWindow = () => {
   myLinkedinWindow = window.open(
     "https://www.linkedin.com/in/jgndongendeba/details/certifications/",
     "_blank",
     "toolbar=yes,scrollbars=yes,resizable=yes,top=150,left=850,width=400,height=200"
   );
-}
-function closeLinkedinWindow() {
+};
+const closeLinkedinWindow = () => {
   myLinkedinWindow.close();
-}
+};
